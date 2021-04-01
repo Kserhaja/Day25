e7bdbcee44e6663512bf417517915b259a230df7
Kserhaja <kenanserhatlic@hotmail.com>
2021-04-01 20:17:49 +0200

    ex01

---
+++ ex01/src/App.js
@@ -0,0 +1,13 @@
+//App.js/GetRequest
+import './App.css';
+import GetRequest from './GetRequest';
+//function App
+function App() {
+  return (
+    <div className="App">
+      <GetRequest />
+    </div>
+  );
+}
+export default App; // export App
+
